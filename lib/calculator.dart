dab32b773346bf7d5fb2233b1a6709933a40a186
mohammed zabith pp <ppzabithmohammed@gmail.com>
2025-05-21 09:55:56 +0530

    1

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -62,7 +62,9 @@ class _CalculatorscreenState extends State<Calculatorscreen> {
             padding: EdgeInsets.all(10),
             backgroundColor: bgcolor ?? Colors.white,
             foregroundColor: fgcolor ?? Colors.black,
-            shape: CircleBorder(side: BorderSide(color: Colors.black)),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
           ),
         ),
       ),
@@ -73,77 +75,100 @@ class _CalculatorscreenState extends State<Calculatorscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.bottomRight,
-              padding: EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    input,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomRight,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      input,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Text(
-                    output,
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w600,
+                    Text(
+                      output,
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 130, 131, 130),
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Row(
-            children: [
-              calculatorbutton('C'),
-              calculatorbutton('%'),
-              calculatorbutton("DEL"),
-              calculatorbutton('/'),
-            ],
-          ),
-          Row(
-            children: [
-              calculatorbutton('7'),
-              calculatorbutton('8'),
-              calculatorbutton('9'),
-              calculatorbutton('X'),
-            ],
-          ),
-          Row(
-            children: [
-              calculatorbutton('4'),
-              calculatorbutton('5'),
-              calculatorbutton('6'),
-              calculatorbutton('-'),
-            ],
-          ),
-          Row(
-            children: [
-              calculatorbutton('1'),
-              calculatorbutton('2'),
-              calculatorbutton('3'),
-              calculatorbutton('+'),
-            ],
-          ),
-          Row(
-            children: [
-              calculatorbutton('00'),
-              calculatorbutton('0'),
-              calculatorbutton('.'),
-              calculatorbutton('='),
-            ],
-          ),
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calculatorbutton('C'),
+                SizedBox(width: 10),
+                calculatorbutton('%'),
+                SizedBox(width: 10),
+                calculatorbutton("DEL"),
+                SizedBox(width: 10),
+                calculatorbutton('÷'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calculatorbutton('7'),
+                SizedBox(width: 10),
+                calculatorbutton('8'),
+                SizedBox(width: 10),
+                calculatorbutton('9'),
+                SizedBox(width: 10),
+                calculatorbutton('×'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calculatorbutton('4'),
+                SizedBox(width: 10),
+                calculatorbutton('5'),
+                SizedBox(width: 10),
+                calculatorbutton('6'),
+                SizedBox(width: 10),
+                calculatorbutton('-'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calculatorbutton('1'),
+                SizedBox(width: 10),
+                calculatorbutton('2'),
+                SizedBox(width: 10),
+                calculatorbutton('3'),
+                SizedBox(width: 10),
+                calculatorbutton('+'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                calculatorbutton('00'),
+                SizedBox(width: 10),
+                calculatorbutton('0'),
+                SizedBox(width: 10),
+                calculatorbutton('.'),
+                SizedBox(width: 10),
+                calculatorbutton('='),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Your App Title',
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: Calculatorscreen(),
+      home: Calculatorscreen(), // Set your default page here
     );
   }
 }
